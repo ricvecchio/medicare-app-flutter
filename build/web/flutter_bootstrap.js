553e6296aa2e0ec1d647d2a59a7e81cedf38c690
ricardo <ric.vecchio@gmail.com>
2025-01-23 21:44:11 -0300

    Limpando e rebuildando o código

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "180235039"
+    serviceWorkerVersion: "3298393479"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "396f7ea8736d9d5f402d63c9ab3f8d45
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "12533a5fa0064b6faa07c30db62084a7",
+"flutter_bootstrap.js": "8cbe242f741ef18d93b4de010a5c903b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
